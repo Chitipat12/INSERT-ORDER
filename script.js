c25958fb303f37700ed3f141c293e594555b06ef
Chitipat12 <Chitipat1999@gmail.com>
2025-06-17 18:17:53 +0700

    Update script.js

--- script.js
+++ script.js
@@ -9,6 +9,9 @@ function validateLogin(event) {
     if (username === 'ADMIN' && password === 'ADMIN') {
         // Redirect to the dashboard if login is successful
         window.location.href = 'dashboard.html'; // หน้า Dashboard
+        if (username === 'SPREECHA' && password === 'SPREECHA') {
+        // Redirect to the dashboard if login is successful
+        window.location.href = 'dashboard.html'; // หน้า Dashboard
     } else {
         // Display error message if login fails
         document.getElementById('error-message').style.display = 'block';
